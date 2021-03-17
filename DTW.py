58158b7b9b4b9beb2eb62f8edf1a5f29f0baff26
Hao <526312824@qq.com>
2021-03-17 12:15:52 -0600

    Update
    plot the solar irradiance data and the energy data. find the DTW distance successfully. Need to find proper method to realign those data

--- DTW.py
+++ DTW.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 
 data=pd.read_csv(r'E:\U_of_A\ECE910\Solar Data (DTWA)\DTW\blatchford_202101.csv',
                       encoding = "ISO-8859-1", engine='python')
+data['date']=pd.to_datetime(data['date'])
 energy=pd.read_csv(r'E:\U_of_A\ECE910\Solar Data (DTWA)\DTW\energy_2021.csv',
                       encoding = "ISO-8859-1", engine='python')
 
-a=data['temperature'][0:24]
-b=data['windSpeed'][0:24]
-#
+#sort the energy by date time and reset index
+energy['date']=pd.to_datetime(energy['date'])
+energy=energy.sort_values(by='date')
+energy = energy.reset_index(drop=True)
+
+a=data['irradiance'][0:24]
+b=energy['energy'][0:71]
+
+#test time series
 #series1 = pd.Series([1,1,4,10,9,3,2,6,8,4])
 #series2 = pd.Series([1,4,10,9,3,2,6,8,4,4])
 
@@ -40,13 +47,13 @@ def dtw_distance(cost_matrix):
     d.append(cost_matrix[row_index,col_index])
     while row_index!=0 and col_index!=0:
         if row_index>0 and col_index>0:
-            if cost_matrix[row_index-1,col_index]<cost_matrix[row_index,col_index-1] and cost_matrix[row_index-1,col_index]<cost_matrix[row_index-1,col_index-1]:
+            if cost_matrix[row_index-1,col_index]<cost_matrix[row_index,col_index-1] and cost_matrix[row_index-1,col_index]<=cost_matrix[row_index-1,col_index-1]:
                 d.append(cost_matrix[row_index-1,col_index])
                 row_index=row_index-1
             elif cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index] and cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index-1]: 
                 d.append(cost_matrix[row_index,col_index-1])
                 col_index=col_index-1
-            elif cost_matrix[row_index-1,col_index-1]<=cost_matrix[row_index-1,col_index] and cost_matrix[row_index-1,col_index-1]<cost_matrix[row_index,col_index-1]: 
+            elif cost_matrix[row_index-1,col_index-1]<=cost_matrix[row_index-1,col_index] and cost_matrix[row_index-1,col_index-1]<=cost_matrix[row_index,col_index-1]: 
                 d.append(cost_matrix[row_index-1,col_index-1])
                 col_index=col_index-1
                 row_index=row_index-1
@@ -57,8 +64,34 @@ def dtw_distance(cost_matrix):
             d.append(cost_matrix[row_index-1,col_index])
             row_index=row_index-1
             
-    print(round(np.sum(d)/len(d),4))
+    print('The DTW distance is: ',round(np.sum(d)/len(d),4))
     
     
 c_matrix=dtw_cost(a,b)
 dtw_distance(c_matrix)
+
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax2=fig.add_subplot(111, frame_on=False)
+
+
+ax.plot(data.iloc[:,1][0:24],data.iloc[:,18][0:24],color="C0");
+ax.set_xlabel("time(every 1 hour)", color="C0")
+ax.set_ylabel("Solar Irradiance", color="C0")
+ax.tick_params(axis='x', colors="C0")
+ax.tick_params(axis='y', colors="C0")
+
+ax2.plot(energy.iloc[:,3][0:71],energy.iloc[:,5][0:71],color="C1");
+ax2.xaxis.tick_top()
+ax2.yaxis.tick_right()
+ax2.set_xlabel('time(every 15 mins)', color="C1") 
+ax2.set_ylabel('Energy', color="C1")       
+ax2.xaxis.set_label_position('top') 
+ax2.yaxis.set_label_position('right') 
+ax2.tick_params(axis='x', colors="C1")
+ax2.tick_params(axis='y', colors="C1")
+
+plt.title('Time series data(solar irradiance and energy)')
+plt.gcf().set_size_inches(20, 10)
+plt.show()  
