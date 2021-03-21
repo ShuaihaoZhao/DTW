489ed30e0394eae7b8d5ad84154896916e090813
Hao <526312824@qq.com>
2021-03-20 19:14:01 -0600

    Update
    realign and visualize the DTW path

--- DTW.py
+++ DTW.py
@@ -13,12 +13,13 @@ energy['date']=pd.to_datetime(energy['date'])
 energy=energy.sort_values(by='date')
 energy = energy.reset_index(drop=True)
 
-a=data['irradiance'][0:24]
-b=energy['energy'][0:71]
+#a=data['irradiance'][0:24]
+#b=energy['energy'][0:71]
 
-#test time series
-#series1 = pd.Series([1,1,4,10,9,3,2,6,8,4])
-#series2 = pd.Series([1,4,10,9,3,2,6,8,4,4])
+
+#test time seriesa
+a = pd.Series([1,2,4,7,8,6,5,3,6,9,7])
+b = pd.Series([1,4,9,11,16,5,4,8,8,4,3])
 
 def dtw_cost(ts1,ts2):
     len_1=len(ts1)
@@ -36,12 +37,13 @@ def dtw_cost(ts1,ts2):
                 matrix[i2,j2]=abs(ts1[i2]-ts2[j2])+matrix[i2,j2-1]
             elif i2 != 0 and j2 == 0:
                 matrix[i2,j2]=abs(ts1[i2]-ts2[j2])+matrix[i2-1,j2]
-    return matrix
 #    print(matrix)
+    return matrix
     
     
 def dtw_distance(cost_matrix):
     d=[]
+    path=[]
     row_index=len(cost_matrix)-1
     col_index=len(cost_matrix[0])-1
     d.append(cost_matrix[row_index,col_index])
@@ -50,48 +52,73 @@ def dtw_distance(cost_matrix):
             if cost_matrix[row_index-1,col_index]<cost_matrix[row_index,col_index-1] and cost_matrix[row_index-1,col_index]<=cost_matrix[row_index-1,col_index-1]:
                 d.append(cost_matrix[row_index-1,col_index])
                 row_index=row_index-1
+                path.append([row_index,col_index])
             elif cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index] and cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index-1]: 
                 d.append(cost_matrix[row_index,col_index-1])
                 col_index=col_index-1
+                path.append([row_index,col_index])
             elif cost_matrix[row_index-1,col_index-1]<=cost_matrix[row_index-1,col_index] and cost_matrix[row_index-1,col_index-1]<=cost_matrix[row_index,col_index-1]: 
                 d.append(cost_matrix[row_index-1,col_index-1])
                 col_index=col_index-1
                 row_index=row_index-1
+                path.append([row_index,col_index])
         elif row_index==0 and col_index>0:
             d.append(cost_matrix[row_index,col_index-1])
             col_index=col_index-1
+            path.append([row_index,col_index])
         elif row_index>0 and col_index==0:
             d.append(cost_matrix[row_index-1,col_index])
             row_index=row_index-1
+            path.append([row_index,col_index])
             
-    print('The DTW distance is: ',round(np.sum(d)/len(d),4))
+#    print('The DTW distance is: ',round(np.sum(d)/len(d),4))
+    print(path)
+    return path
     
     
 c_matrix=dtw_cost(a,b)
-dtw_distance(c_matrix)
+path_pair=dtw_distance(c_matrix)
+
+plt.figure(figsize=(20,10))
+plt.subplot()
+plt.plot(a.index,a)
+plt.plot(b.index,b)
 
 
-fig=plt.figure()
-ax=fig.add_subplot(111)
-ax2=fig.add_subplot(111, frame_on=False)
+#fig=plt.figure()
+#ax=fig.add_subplot(111)
+#ax2=fig.add_subplot(111, frame_on=False)
+#
+#
+#ax.plot(data.iloc[:,1][0:24],data.iloc[:,18][0:24],color="C0");
+#ax.set_xlabel("time(every 1 hour)", color="C0")
+#ax.set_ylabel("Solar Irradiance", color="C0")
+#ax.tick_params(axis='x', colors="C0")
+#ax.tick_params(axis='y', colors="C0")
+#
+#ax2.plot(energy.iloc[:,3][0:71],energy.iloc[:,5][0:71],color="C1");
+#ax2.xaxis.tick_top()
+#ax2.yaxis.tick_right()
+#ax2.set_xlabel('time(every 15 mins)', color="C1") 
+#ax2.set_ylabel('Energy', color="C1")       
+#ax2.xaxis.set_label_position('top') 
+#ax2.yaxis.set_label_position('right') 
+#ax2.tick_params(axis='x', colors="C1")
+#ax2.tick_params(axis='y', colors="C1")
+#
+#plt.title('Time series data(solar irradiance and energy)')
+#plt.gcf().set_size_inches(20, 10)
 
-
-ax.plot(data.iloc[:,1][0:24],data.iloc[:,18][0:24],color="C0");
-ax.set_xlabel("time(every 1 hour)", color="C0")
-ax.set_ylabel("Solar Irradiance", color="C0")
-ax.tick_params(axis='x', colors="C0")
-ax.tick_params(axis='y', colors="C0")
-
-ax2.plot(energy.iloc[:,3][0:71],energy.iloc[:,5][0:71],color="C1");
-ax2.xaxis.tick_top()
-ax2.yaxis.tick_right()
-ax2.set_xlabel('time(every 15 mins)', color="C1") 
-ax2.set_ylabel('Energy', color="C1")       
-ax2.xaxis.set_label_position('top') 
-ax2.yaxis.set_label_position('right') 
-ax2.tick_params(axis='x', colors="C1")
-ax2.tick_params(axis='y', colors="C1")
-
-plt.title('Time series data(solar irradiance and energy)')
-plt.gcf().set_size_inches(20, 10)
+for i in range(len(path_pair)):
+    x1=path_pair[i][0]
+    x2=path_pair[i][1]
+    y1=a[x1]
+    y2=b[x2]
+    temp=[[x1,x2],[y1,y2]]
+    plt.plot(temp[0],temp[1],'--',color='k')
+    
 plt.show()  
+    
+#plt.figure(figsize=(20,10))
+#plt.plot(energy.iloc[:,3][0:578],energy.iloc[:,5][0:578]/15,color="C0");
+#plt.plot(data.iloc[:,1][0:168],data.iloc[:,18][0:168],color="C1");
