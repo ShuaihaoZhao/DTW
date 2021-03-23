94d52351ae3bb2d53e110348d0bff794f7b7e676
Hao <526312824@qq.com>
2021-03-23 14:46:45 -0600

    Update
    Add the heatmap for the DTW, data matching performs good for simple data but provides wired matching behaviors for complex data sets. No idea which part is wrong. Need to improve the matching algorithm for better performance

--- DTW.py
+++ DTW.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 data=pd.read_csv(r'E:\U_of_A\ECE910\Solar Data (DTWA)\DTW\blatchford_202101.csv',
                       encoding = "ISO-8859-1", engine='python')
@@ -8,19 +9,29 @@ data['date']=pd.to_datetime(data['date'])
 energy=pd.read_csv(r'E:\U_of_A\ECE910\Solar Data (DTWA)\DTW\energy_2021.csv',
                       encoding = "ISO-8859-1", engine='python')
 
+energy_5=pd.read_csv(r'E:\U_of_A\ECE910\Solar Data (DTWA)\DTW\energy_5_sites.csv',
+                      encoding = "ISO-8859-1", engine='python')[0:3102]
+
 #sort the energy by date time and reset index
 energy['date']=pd.to_datetime(energy['date'])
 energy=energy.sort_values(by='date')
 energy = energy.reset_index(drop=True)
 
+energy_5['date']=pd.to_datetime(energy_5['date'])
+energy_5=energy_5.sort_values(by='date')
+energy_5=energy_5.reset_index(drop=True)
+
+
+
 #a=data['irradiance'][0:24]
-#b=energy['energy'][0:71]
+#a=energy['energy'][0:48]/26280
+#b=energy_5['energy'][0:34]/6896
 
 
 #test time seriesa
-a = pd.Series([1,2,4,7,8,6,5,3,6,9,7])
-b = pd.Series([1,4,9,11,16,5,4,8,8,4,3])
-
+a = pd.Series([1,3,5,5,6,8,9,10,11,7,5,6,5,6,2,1])
+b = pd.Series([0,0,0,0,0,0,0,0,2,4,6,6,6,7,9,10,10,11,12,8,6,5,4,5,1,0])
+1
 def dtw_cost(ts1,ts2):
     len_1=len(ts1)
     len_2=len(ts2)
@@ -47,13 +58,14 @@ def dtw_distance(cost_matrix):
     row_index=len(cost_matrix)-1
     col_index=len(cost_matrix[0])-1
     d.append(cost_matrix[row_index,col_index])
-    while row_index!=0 and col_index!=0:
+    path.append([row_index,col_index])
+    while row_index!=0 or col_index!=0:
         if row_index>0 and col_index>0:
-            if cost_matrix[row_index-1,col_index]<cost_matrix[row_index,col_index-1] and cost_matrix[row_index-1,col_index]<=cost_matrix[row_index-1,col_index-1]:
+            if cost_matrix[row_index-1,col_index]<cost_matrix[row_index,col_index-1] and cost_matrix[row_index-1,col_index]<cost_matrix[row_index-1,col_index-1]:
                 d.append(cost_matrix[row_index-1,col_index])
                 row_index=row_index-1
                 path.append([row_index,col_index])
-            elif cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index] and cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index-1]: 
+            elif cost_matrix[row_index,col_index-1]<=cost_matrix[row_index-1,col_index] and cost_matrix[row_index,col_index-1]<cost_matrix[row_index-1,col_index-1]: 
                 d.append(cost_matrix[row_index,col_index-1])
                 col_index=col_index-1
                 path.append([row_index,col_index])
@@ -71,43 +83,36 @@ def dtw_distance(cost_matrix):
             row_index=row_index-1
             path.append([row_index,col_index])
             
-#    print('The DTW distance is: ',round(np.sum(d)/len(d),4))
-    print(path)
+    print('The DTW distance is: ',round(np.sum(d)/len(d),4))
+#    print(path)
+#    print(d)
     return path
     
-    
+
 c_matrix=dtw_cost(a,b)
 path_pair=dtw_distance(c_matrix)
 
-plt.figure(figsize=(20,10))
-plt.subplot()
-plt.plot(a.index,a)
-plt.plot(b.index,b)
+path_x = [p[1] for p in path_pair]
+path_y = [p[0] for p in path_pair]
 
+path_xx = [x+0.5 for x in path_x]
+path_yy = [y+0.5 for y in path_y]
 
-#fig=plt.figure()
-#ax=fig.add_subplot(111)
-#ax2=fig.add_subplot(111, frame_on=False)
-#
-#
-#ax.plot(data.iloc[:,1][0:24],data.iloc[:,18][0:24],color="C0");
-#ax.set_xlabel("time(every 1 hour)", color="C0")
-#ax.set_ylabel("Solar Irradiance", color="C0")
-#ax.tick_params(axis='x', colors="C0")
-#ax.tick_params(axis='y', colors="C0")
-#
-#ax2.plot(energy.iloc[:,3][0:71],energy.iloc[:,5][0:71],color="C1");
-#ax2.xaxis.tick_top()
-#ax2.yaxis.tick_right()
-#ax2.set_xlabel('time(every 15 mins)', color="C1") 
-#ax2.set_ylabel('Energy', color="C1")       
-#ax2.xaxis.set_label_position('top') 
-#ax2.yaxis.set_label_position('right') 
-#ax2.tick_params(axis='x', colors="C1")
-#ax2.tick_params(axis='y', colors="C1")
-#
-#plt.title('Time series data(solar irradiance and energy)')
-#plt.gcf().set_size_inches(20, 10)
+fig, ax = plt.subplots(figsize=(12, 12))
+ax = sns.heatmap(c_matrix, cmap="YlGnBu",square=True, linewidths=0.1,ax=ax)
+bottom, top = ax.get_ylim()
+ax.set_ylim(bottom + 0.5, top - 0.5)
+ax.invert_yaxis()
+
+ax.plot(path_xx, path_yy, color='red', linewidth=3, alpha=0.5)
+
+plt.figure(figsize=(12,12))
+plt.plot(a.index,a,color='b',label='date set 1')
+plt.plot(b.index,b,color='r',label='date set 1')
+plt.title('Energy Data Plot')
+plt.xlabel('Index')
+plt.ylabel('Normalized')
+plt.legend(loc="upper left")
 
 for i in range(len(path_pair)):
     x1=path_pair[i][0]
@@ -115,10 +120,8 @@ for i in range(len(path_pair)):
     y1=a[x1]
     y2=b[x2]
     temp=[[x1,x2],[y1,y2]]
-    plt.plot(temp[0],temp[1],'--',color='k')
+    plt.scatter(temp[0],temp[1],marker='o',color='k');
+    plt.plot(temp[0],temp[1],'--',color='k',alpha=0.3)
     
 plt.show()  
     
-#plt.figure(figsize=(20,10))
-#plt.plot(energy.iloc[:,3][0:578],energy.iloc[:,5][0:578]/15,color="C0");
-#plt.plot(data.iloc[:,1][0:168],data.iloc[:,18][0:168],color="C1");
